1579d2b5a528bf04f094d2f2c3532a32dff5ab4f
xkcm <xkcm16@gmail.com>
2023-10-19 11:06:07 +0200

    Init Vitepress project

---
+++ .vitepress/config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "xkcm portfolio",
+  description: "A Portfolio Page of xkcm solutions"
+})
